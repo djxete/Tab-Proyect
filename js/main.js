204e996ac90168fdc2b7b5bccc7ab231cc4d82db
Ignacio Herráez <ignacio_hg_87@hotmail.com>
2020-12-15 20:02:04 +0100

    proyect finished

--- js/main.js
+++ js/main.js
@@ -1,1 +1,49 @@
-alert(`testing`);
+
+
+//=================== VARIABLES ========
+
+const btns = document.querySelectorAll(".tab-btn");
+const about = document.querySelector(".about");
+const articles = document.querySelectorAll(".content");
+
+
+
+
+
+about.addEventListener("click", e => {
+
+
+    let btnClickeado = e.target.getAttribute("data-id");
+
+    if (btnClickeado != undefined) {
+        
+        // remove active from other buttons
+
+        btns.forEach(btn =>{
+            btn.classList.remove("active");
+            e.target.classList.add("active");
+        })
+
+        // hide all articles
+
+        articles.forEach(article =>{
+            article.classList.remove("active");
+            
+        })
+
+        //show article depends on the button you clicked on
+
+        const element = document.getElementById(btnClickeado);
+        
+        element.classList.add("active");
+
+
+    }
+    
+    else{
+        console.log(`No es un botón`);
+    }
+})
+
+
+
